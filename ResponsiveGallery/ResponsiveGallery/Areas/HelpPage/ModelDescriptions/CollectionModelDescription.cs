0d9320af433a8369c9a3f061951cd2197415f7bb
Alexander Thomas Williams <cerafem@gmail.com>
2016-08-25 12:59:41 -0400

    Initial Project Files

---
+++ ResponsiveGallery/ResponsiveGallery/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace ResponsiveGallery.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ ResponsiveGallery/ResponsiveGallery/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ResponsiveGallery.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
