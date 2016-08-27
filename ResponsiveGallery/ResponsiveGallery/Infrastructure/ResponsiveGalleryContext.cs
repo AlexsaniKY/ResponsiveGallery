e4a1acd0d69ee0e51c570b2cddaf037b5dd43980
AlexsaniKY <cerafem@gmail.com>
2016-08-27 17:40:42 -0400

    installed EF and added context

---
+++ ResponsiveGallery/ResponsiveGallery/Infrastructure/ResponsiveGalleryContext.cs
@@ -0,0 +1,25 @@
+﻿using ResponsiveGallery.Domain;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ResponsiveGallery.Infrastructure
+{
+    public class ResponsiveGalleryContext : DbContext
+    {
+        public ResponsiveGalleryContext(): base("ResponsiveGalleryContext")
+        {
+            Database.SetInitializer<ResponsiveGalleryContext>(null);
+        }
+
+        public static ResponsiveGalleryContext Create()
+        {
+            return new ResponsiveGalleryContext();
+        }
+
+        public DbSet<GalleryImage> GalleryImages { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
