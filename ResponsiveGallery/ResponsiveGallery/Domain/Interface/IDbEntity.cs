755a846d72397b2740ed4070705402687d2ea1e3
AlexsaniKY <cerafem@gmail.com>
2016-08-27 13:44:41 -0400

    Began domain models, Db interface

---
+++ ResponsiveGallery/ResponsiveGallery/Domain/Interface/IDbEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiveGallery.Domain.Interface
+{
+    interface IDbEntity
+    {
+        int Id { get; set; }
+        bool Active { get; set; }
+    }
+}
