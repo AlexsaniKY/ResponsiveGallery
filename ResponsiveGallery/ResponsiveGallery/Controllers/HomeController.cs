77455cec0a052b8414e8a7e4d915c4b894f87efe
AlexsaniKY <cerafem@gmail.com>
2016-10-20 09:15:39 -0400

    converting inline styles to use style sheet

--- ResponsiveGallery/ResponsiveGallery/Controllers/HomeController.cs
+++ ResponsiveGallery/ResponsiveGallery/Controllers/HomeController.cs
@@ -14,7 +14,13 @@ namespace ResponsiveGallery.Controllers
             ViewBag.Title = "Home Page";
             List<GalleryImage> images = new List<GalleryImage>();
             GalleryImage testimage = new GalleryImage(true, "Cat Image", Url.Content("~/Images/DSC00885.jpg"), new List<Tag>() ) ;
+            GalleryImage longnameimage = new GalleryImage(true, "Another Cat Image but this time with a really long title", Url.Content("~/Images/DSC00885.jpg"), new List<Tag>());
+            images.Add(longnameimage);
             for (int i = 0; i < 10; i++)
+                images.Add(testimage);
+            images.Add(longnameimage);
+            images.Add(longnameimage);
+            for (int i = 0; i < 15; i++)
                 images.Add(testimage);
             return View(images);
         }
